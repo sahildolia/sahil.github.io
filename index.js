48a531ab621e51d81337558e4c0c3fa78b2a03cc
sahildolia <sahildolia.456@gmail.com>
2022-02-11 12:51:30 +0530

    nav-correct

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 burger = document.querySelector(`.burger`)
 navList = document.querySelector(`.nav-list`)
 navbar = document.querySelector(`.navbar`)
+navList = document.querySelector(`.nav-list`)
+
 
 burger.addEventListener(`click`,()=>{
     navList.classList.toggle(`v-class-resp`)
     navbar.classList.toggle(`h-nav-resp`)
 })
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
