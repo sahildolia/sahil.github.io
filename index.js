1e4213b86a3c153be5266433f5989695fbf336ef
sahildolia <sahildolia.456@gmail.com>
2022-02-11 12:55:22 +0530

    changed

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+// burger = document.querySelector(`.burger`)
+// navList = document.querySelector(`.nav-list`)
+// navbar = document.querySelector(`.navbar`)
+// navList = document.querySelector(`.nav-list`)
+
+
+// burger.addEventListener(`click`,()=>{
+//     navList.classList.toggle(`v-class-resp`)
+//     navbar.classList.toggle(`h-nav-resp`)
+// })
+
+
 burger = document.querySelector(`.burger`)
-navList = document.querySelector(`.nav-list`)
 navbar = document.querySelector(`.navbar`)
 navList = document.querySelector(`.nav-list`)
 
 
-burger.addEventListener(`click`,()=>{
-    navList.classList.toggle(`v-class-resp`)
-    navbar.classList.toggle(`h-nav-resp`)
-})
+burger.addEventListener(`click`, ()=>{
+    navList.classList.toggle(`v-class-resp`);
+    navbar.classList.toggle(`h-nav-resp`);
+    
+    })
